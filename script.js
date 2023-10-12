3c94ee79b5f824da4e53ba598becf8b2aba59d4b
Juliet <musyoki.juliet011@gmail.com>
2023-10-12 20:22:36 +0300

    DONE

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function clickHandler(e){
         behavior: "smooth"
     });
 }
-fetch(apiUrl) 
+fetch(apiUrl)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
