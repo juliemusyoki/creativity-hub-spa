fe3617314f7bdd594d4cb9e910783e21b8ec5013
Juliet <musyoki.juliet011@gmail.com>
2023-10-15 15:35:16 +0300

    DONE

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const app = express();
+const jsonServer = require('json-server')
 
-app.get('/', (req, res) => {
-  res.send('Hello, this is your backend!');
-});
+const server = jsonServer.create()
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+ 
+server.use(middlewares)
+server.use('/', router)
+server.listen(process.env.PORT || 3000, () => {
+  console.log('JSON Server is running')
+})
